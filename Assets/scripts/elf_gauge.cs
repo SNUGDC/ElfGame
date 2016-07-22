2f4f8895d2cf37f674bd58dd27d68c17c799533d
JeongMunKyu <amigojmk@naver.com>
2016-07-23 00:22:00 +0900

    체력바 구현
    체력바 왼쪽 위에 구현
    시간이 지날 때마다 체력바 줄어드는 것 구현

--- Assets/scripts/elf_gauge.cs
+++ Assets/scripts/elf_gauge.cs
@@ -3,28 +3,47 @@ using System.Collections;
 
 public class elf_gauge : MonoBehaviour 
 {
-    public int health;
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
+    public float healthDownforTime = 5f;
     public Hashtable quest;
+    public GameObject healthFill;
+    float healthPercent;
+    Vector3 healthBarSize;
+    Vector3 healthBarMove;
     int damage;
     int healing;
+
+
 	// Use this for initialization
 	void Start () 
     {
-        health = 2;
+        //health = 2;              윤아 확인바람
         quest = new Hashtable();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    
-	}
+        currentHealth -= Time.deltaTime * healthDownforTime;
+        healthPercent = currentHealth / maxHealth;
+        if (healthPercent < 0f)
+        {
+            healthPercent = 0f;
+        }
+        print(currentHealth);
+        healthBarSize = new Vector3(currentHealth / 20, 0.3f, 0.1f);
+        healthBarMove = new Vector3(-6f - (100 - currentHealth) / 40, 3.3f, -1f);
+        healthFill.transform.localScale = healthBarSize;
+        healthFill.transform.localPosition = transform.position + healthBarMove;
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.GetComponent<Collider2D>().tag == "NPC")
         {
             healing = col.gameObject.GetComponent<NPC>().heal;
-            health += healing;
+            currentHealth += (float)healing;
         }
         if (col.GetComponent<Collider2D>().tag == "quest_item")
         {
@@ -33,7 +52,7 @@ public class elf_gauge : MonoBehaviour
         if (col.GetComponent<Collider2D>().tag == "enemy")
         {
             damage = col.gameObject.GetComponent<enemy>().power;
-            health -= damage;
+            currentHealth -= (float)damage;
         }
     }
 }
